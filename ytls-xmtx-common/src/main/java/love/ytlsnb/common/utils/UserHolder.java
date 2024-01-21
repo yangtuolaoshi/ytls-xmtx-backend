38d6723556afa782512cd9e5c291dded6224e129
ytls <2371083008@qq.com>
2024-01-21 21:46:59 +0800

    项目基本结构

---
+++ ytls-xmtx-common/src/main/java/love/ytlsnb/common/utils/UserHolder.java
@@ -0,0 +1,39 @@
+package love.ytlsnb.common.utils;
+
+import love.ytlsnb.model.user.pojo.User;
+
+/**
+ * APP用户状态保存
+ *
+ * @author 金泓宇
+ * @date 2024/01/21
+ */
+public class UserHolder {
+    /**
+     * 独立线程保存用户状态
+     */
+    private static final ThreadLocal<User> tl = new ThreadLocal<>();
+
+    /**
+     * 保存用户信息
+     * @param user 用户信息
+     */
+    public static void saveUser(User user){
+        tl.set(user);
+    }
+
+    /**
+     * 获取用户信息
+     * @return 用户信息
+     */
+    public static User getUser(){
+        return tl.get();
+    }
+
+    /**
+     * 移出用户信息
+     */
+    public static void removeUser(){
+        tl.remove();
+    }
+}
--- ytls-xmtx-gateway/ytls-xmtx-app-gateway/src/main/java/love/ytls/app/gateway/AppGatewayApplication.java
+++ ytls-xmtx-gateway/ytls-xmtx-app-gateway/src/main/java/love/ytls/app/gateway/AppGatewayApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 用户APP网关启动类
+ *
+ * @author 金泓宇
+ * @date 2024/01/21
+ */
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AppGatewayApplication {
