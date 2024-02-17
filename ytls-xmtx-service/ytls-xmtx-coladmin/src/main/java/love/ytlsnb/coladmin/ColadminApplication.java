9aec3c95ea53b7eef039a7a11939feba3b71a03f
ytls <2371083008@qq.com>
2024-02-17 20:12:23 +0800

    OSS工具类——新增、删除方法

---
+++ ytls-xmtx-service/ytls-xmtx-coladmin/src/main/java/love/ytlsnb/coladmin/ColadminApplication.java
@@ -0,0 +1,11 @@
+package love.ytlsnb.coladmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColadminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ColadminApplication.class, args);
+    }
+}
