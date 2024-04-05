68fd757acdeca162307b26952cee1c1cc86cb4f1
linyan <linyan20030529@163.com>
2024-04-05 12:47:34 +0800

    自动检查BUG修复

--- ytls-xmtx-model/src/main/java/love/ytlsnb/model/ad/po/Advertisement.java
+++ ytls-xmtx-model/src/main/java/love/ytlsnb/model/ad/po/Advertisement.java
@@ -66,7 +66,9 @@ public class Advertisement {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
